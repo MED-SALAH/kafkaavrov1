4af409cb2f7e18b7f8afb156c8f66d2056502bb0
MED-SALAH <32021669+MED-SALAH@users.noreply.github.com>
2020-05-08 20:11:10 +0200

    eventheader

--- src/main/java/com/example/KafkaAvroProducerv1.java
+++ src/main/java/com/example/KafkaAvroProducerv1.java
@@ -25,7 +25,8 @@ public class KafkaAvroProducerv1 {
 
         EventHeader eventHeader = EventHeader.newBuilder()
                 .setEventId("000012")
-                .setHeaderVersion("v1").setDateTimeRef(1345234020)
+                .setHeaderVersion("v1")
+                .setDateTimeRef(1345234020)
                 .setNomenclatureEv("Code Nomenclature de l'événement")
                 .setSchemaVersion("version")
                 .setServeur("serveur")
